5ee6668482b6ba4b33d6fca4f7d7f9a2742e6923
Chengxiao Gong <gcx@mail.nwpu.edu.cn>
2023-06-04 18:00:52 +0800

    增加类别信息

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -6,16 +6,18 @@ interface BoxProps {
     className?: string,
     image_src: string,
     title: string,
+    category: string,
     image_id: number,
     caption_en: string,
     caption_zh: string,
-    onSubmit: () => Promise<void>,
+    onSubmit: (value: number) => Promise<void>,
 }
 
 const Box: React.FC<BoxProps> = ({
     className,
     image_src,
     title,
+    category,
     image_id,
     caption_en,
     caption_zh,
@@ -24,14 +26,15 @@ const Box: React.FC<BoxProps> = ({
 }) => {
 
     return (
-        <div className={twMerge('w-full flex-col flex items-start rounded-md border p-4 hover:bg-slate-100 shadow-md',
+        <div className={twMerge('w-full flex-col flex items-start rounded-md border p-4 hover:bg-slate-100 shadow-md hover:shadow-lg',
             className)}>
             <div className="flex flex-row mt-6 ">
                 <Image className="hover:brightness-110" src={image_src}
                     alt="commercial_area_001.jpg" width={300} height={300} />
                 <div className="pl-8 flex flex-col">
-                    <h1 className="text-xl font-bold">{title}</h1>
+                    <h1 className="text-xl font-bold mb-2">{title}</h1>
                     <p>Image ID: {image_id}</p>
+                    <p>Category: {category}</p>
                 </div>
 
             </div>
@@ -51,7 +54,7 @@ const Box: React.FC<BoxProps> = ({
 
             <div className="flex justify-end w-full mt-4 pr-4">
                 <button className="bg-green-500 text-white rounded-md w-24 h-10 hover:bg-green-400"
-                    onClick={onSubmit}>
+                    onClick={() => onSubmit(1)}>
                     提交
                 </button>
             </div>
